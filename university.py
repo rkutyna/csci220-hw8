fef06e9705553c3160b2f68d02d592b4dd044064
Colin Ruiz <CRuiz@clarku.edu>
2024-03-12 18:30:39 -0400

    update

--- university.py
+++ university.py
@@ -5,13 +5,43 @@ import sys
 
 def show_students(conn):
     cur=conn.cursor()
-    cur.execute('SELECT id, name, dept FROM student LEFT OUTER JOIN majors_in ON student.id=majors_in.student')
-    print(f'ID|Name|Major')
-    for row in cur:
-        id, name, major = row
-        if major==None:
-            major = 'Undeclared'
-        print(f'{id}|{name}|{major}')
+    cur.execute('SELECT id, name FROM student')
+    body = """
+    <h2>Profile List</h2>
+    <p>
+    <table border=1>
+      <tr>
+        <td><font size=+1"><b>id</b></font></td>
+        <td><font size=+1"><b>name</b></font></td>
+      </tr>
+    """
+    
+    count = 0
+    # each iteration of this loop creates on row of output:
+    for id, name in cur:
+        body += (
+            "<tr>"
+            f"<td><a href='?idNum={id}'>{name}</a></td>"
+            "<td><form method='post' action='university.py'>"
+            f"<input type='hidden' NAME='idNum' VALUE='{id}'>"
+            '<input type="submit" name="deleteProfile" value="Delete">'
+            "</form></td>"
+            "</tr>\n"
+        )
+        count += 1
+
+    body += "</table>" f"<p>Found {count} profiles.</p>"
+    
+    return body
+    
+def deleteProfile(conn, idNum):
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM student WHERE id = %s", (idNum,))
+    conn.commit()
+    if cursor.rowcount > 0:
+        return "Delete Profile Succeeded."
+    else:
+        return "Delete Profile Failed."
 
 
 def show_enrollments(conn):
