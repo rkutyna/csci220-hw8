db9c93329746fbf9f6c2963f6387d29976a765a9
rkutyna <67471832+rkutyna@users.noreply.github.com>
2024-03-12 15:54:29 -0400

    Create university.py

---
+++ university.py
@@ -0,0 +1,108 @@
+import argparse
+import sqlite3
+import sys
+
+
+def show_students(conn):
+    cur=conn.cursor()
+    cur.execute('SELECT id, name, dept FROM student LEFT OUTER JOIN majors_in ON student.id=majors_in.student')
+    print(f'ID|Name|Major')
+    for row in cur:
+        id, name, major = row
+        if major==None:
+            major = 'Undeclared'
+        print(f'{id}|{name}|{major}')
+
+
+def show_enrollments(conn):
+    try:
+        cur=conn.cursor()
+        cur.execute('SELECT number, COUNT(student) FROM course LEFT JOIN enrolled ON enrolled.course = course.number GROUP BY number')
+        print(f'Number|Enrollment')
+        for row in cur:
+            number, total = row
+            print(f'{number}|{total}')
+    except Exception as e:
+        print(e)
+        
+
+def add_student(conn, id, name):
+    try:
+        cur=conn.cursor()
+        cur.execute('INSERT INTO student VALUES (?,?)', (id, name))
+        conn.commit()
+    except sqlite3.IntegrityError as e:
+        sys.stderr.write(str(e) + '\n')
+
+
+def enroll_student(conn, student, course):
+    try:
+        cur=conn.cursor()
+        cur.execute('INSERT INTO enrolled VALUES (?,?)', (student, course))
+        conn.commit()
+    except sqlite3.IntegrityError as e:
+        sys.stderr.write(str(e) + '\n')
+
+
+def declare_major(conn, student, department):
+    try:
+        cur=conn.cursor()
+        cur.execute('INSERT INTO majors_in VALUES (?,?)', (student, department))
+        conn.commit()
+    except sqlite3.IntegrityError as e:
+        sys.stderr.write(str(e) + '\n')
+
+
+def main():
+    argparser = argparse.ArgumentParser(
+        description="Interact with the university database"
+    )
+    argparser.add_argument(
+        "database",
+        help="SQLite database file",
+    )
+    argparser.add_argument(
+        "--add-student",
+        metavar=("ID", "NAME"),
+        nargs=2,
+        help="Add a student",
+    )
+    argparser.add_argument(
+        "--enroll-student",
+        metavar=("STUDENT", "COURSE"),
+        nargs=2,
+        help="Enroll a student in a course",
+    )
+    argparser.add_argument(
+        "--declare-major",
+        metavar=("STUDENT", "DEPARTMENT"),
+        nargs=2,
+        help="Declare a student's major",
+    )
+    argparser.add_argument(
+        "--show-students",
+        action="store_true",
+        help="List all students IDs, names, and their majors",
+    )
+    argparser.add_argument(
+        "--show-enrollments",
+        action="store_true",
+        help="List all courses and the number of students enrolled",
+    )
+    args = argparser.parse_args()
+    conn = sqlite3.connect(args.database)
+    conn.execute("PRAGMA foreign_keys = ON")
+    if args.add_student:
+        add_student(conn, *args.add_student)
+    if args.enroll_student:
+        enroll_student(conn, *args.enroll_student)
+    if args.declare_major:
+        declare_major(conn, *args.declare_major)
+    if args.show_students:
+        show_students(conn)
+    if args.show_enrollments:
+        show_enrollments(conn)
+
+
+if __name__ == "__main__":
+    main()
